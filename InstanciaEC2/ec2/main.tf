d14dae4f029ec5cc8124e221965d9cfa2d42b4b9
jmurdock29 <johan159753456@outlook.com>
2022-11-13 23:01:11 -0500

    Subiendo Laboratorio

---
+++ InstanciaEC2/ec2/main.tf
@@ -0,0 +1,15 @@
+
+provider "aws" {
+  region = "eu-west-2"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0648ea225c13e0729"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ec2ServerTerraform"
+  }
+}
